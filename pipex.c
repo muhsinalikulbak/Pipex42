e74068d9780b4dafb5bf446cb8c76e1641d5cfeb
muhsinalikulbak <mkulbak@k1m07s07.42istanbul.com.tr>
2025-01-18 22:46:37 +0300

    dosyadan okuma işlemi grep

--- pipex.c
+++ pipex.c
@@ -1,36 +1,39 @@
-#include <stdio.h> 
-#include <unistd.h>
-#include <string.h> 
-#include <errno.h>
-#include <fcntl.h>
-#include <stdlib.h>
-
+#include "pipex.h"
 
 int main()
 { 
     int pipefd[2];
+    int pid;
+    int fd;
+    int outfd;
     pipe(pipefd);
-    int pid = fork();
+    fd = open("infile.txt", O_RDONLY,0644);
+    pid = fork();
+
     if (pid == 0)
     {
-        close(pipefd[1]);
-        dup2(pipefd[0],STDIN_FILENO);
-
-        char *args[] = {"/bin/grep","pipex",NULL};
-        char *env[] = {NULL};
-        execve("/bin/grep", args,env);
-        close(pipefd[0]);
-    }
-    if (pid > 0)
-    {
+        dup2(fd,STDIN_FILENO);
         dup2(pipefd[1],STDOUT_FILENO);
-        close(pipefd[0]);
-        char *args[] = {"/bin/ls" ,NULL};
-        char *env[] = {NULL};
-
-        if (execve("/bin/ls",args,env) == -1)
-            perror("execve hatasi");
         close(pipefd[1]);
+        close(pipefd[0]);
+        close(fd);
+        char    *argv[] = {"bin/ls",NULL};
+        char    *env[] = {NULL};
+        execve("/bin/ls",argv,env);
     }
-}
+    else
+    {
+        wait(NULL);
+        outfd = open("outfile", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+        dup2(outfd,STDOUT_FILENO);
+        dup2(pipefd[0],STDIN_FILENO);
+        close(outfd);
+        close(pipefd[0]);
+        close(pipefd[1]);
 
+        char    *argv[] = {"bin/grep","M",NULL};
+        char    *env[] = {NULL};
+        execve("/bin/grep",argv,env);
+    }
+    return 0;  
+}
---
+++ pipex.h
@@ -0,0 +1,7 @@
+#include <stdio.h> 
+#include <unistd.h>
+#include <string.h> 
+#include <errno.h>
+#include <fcntl.h>
+#include <stdlib.h>
+# include <sys/wait.h>
